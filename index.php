be9194f7dd5126d07504dc42a97bfd36a3e9a979
t6kela02 <lasse.kelottijarvi@gmail.com>
2019-03-19 20:39:53 +0200

    First upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lasse Kelottijärvi</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<nav id="navbar">
+		<div class="container">
+			<ul>
+				<li><a href="#App">App</a></li>
+				<li><a href="#Data">Data</a></li>
+				<li><a href="#">Home</a></li>
+			</ul>
+		</div>
+	</nav>
+	
+	<header id="main-header">
+		<div class="container">
+			<h1>Tervetuloa!</h1>
+		</div>
+	</header>
+	
+	<div class="whitetextarea">
+		<section id="main">
+			<h1>Moi</h1>
+			<p>
+			The project uses Estimote beacons, which the Android application recognises using bluethooth. 
+			The application saves the time spent near a certain beacon to the database and user can view own or all users spent time.
+			</p>
+		</section>
+	</div>
+	
+	<footer id="main-footer">
+		<p class="container">&copy 2019</p>
+	</footer>
+</body>
+</html>
